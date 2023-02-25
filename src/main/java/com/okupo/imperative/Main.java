08199ffa8888768c1710734cc351d0ae3ec5a04c
Renato Oliveira <renato.dev.oliveira@gmail.com>
2023-02-25 14:09:28 -0300

    Imperative approach

--- src/main/java/com/okupo/imperative/Main.java
+++ src/main/java/com/okupo/imperative/Main.java
@@ -1,8 +1,47 @@
 package com.okupo.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Person> people = List.of(
+                new Person("John", Gender.MALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Aisha", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Alice", Gender.FEMALE));
+
+        // Imperative approach
+        List<Person> females = new ArrayList<Person>();
+
+        for (Person person : people) {
+            if (Gender.FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
     }
 
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [name=" + name + ", gender=" + gender + "]";
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
 }
